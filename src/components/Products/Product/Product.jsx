19e9496ceb6eb92d1d7aac73b79bd29b703c12e6
Ruslan <rus@gmx.com>
2021-03-31 00:45:06 +0100

    Add price to products

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 import classes from '*.module.css';
 
 function Product() {
-    return (
-        <Card className={classes.root}>
-            <CardMedia className={classes.media} image={} />
-        </Card>
-    )
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image="" />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="h5"></Typography>
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default Product;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,8 +3,8 @@ import Grid, { Typography } from '@material-ui/core';
 import classes from '*.module.css';
 
 const products = [
-  { id: 1, name: 'Shoes', description: 'Running shoes.' },
-  { id: 2, name: 'Macbook', description: 'Apple macbook' },
+  { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5' },
+  { id: 2, name: 'Macbook', description: 'Apple macbook', price: '$10' },
 ];
 
 const Products = () => {
@@ -13,17 +13,17 @@ const Products = () => {
       {products.map((product) => {
         <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
           <Products product={product} />
-          <CardContent>
+          {/* <CardContent>
             <div className={classes.CardContent}>
               <Typography variant="h5" gutterBottom>
                 {product.name}
               </Typography>
             </div>
-          </CardContent>
+          </CardContent> */}
         </Grid>;
       })}
     </Grid>
   </main>;
-};
+}; 
 
 export default Products;
