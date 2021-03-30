bcca782a3b208c5660a280b789608f2623095205
Ruslan <rus@gmx.com>
2021-03-31 00:58:39 +0100

    Add .media style

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -2,6 +2,10 @@ import {makeStyles} from '@material-ui/core/styles'
 
 export default makeStyles(() => ({
     root: {
-        maxWidth: '100%'
+        maxWidth: '100%',
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', //16:9
     }
 }));
