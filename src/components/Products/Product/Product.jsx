38dc9471ae4bd6b637c700412c43e6701b0b8134
Ruslan <rus@gmx.com>
2021-03-31 00:48:31 +0100

    Add typography h2

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -10,14 +10,18 @@ import {
 import { AddShoppingCart } from '@material-ui/icons';
 import classes from '*.module.css';
 
-function Product() {
+function Product({ product }) {
   return (
     <Card className={classes.root}>
       <CardMedia className={classes.media} image="" />
       <CardContent>
         <div className={classes.cardContent}>
-          <Typography variant="h5"></Typography>
+          <Typography variant="h5" gutterbottom>
+            {product.name}
+          </Typography>
+          <Typography variant="h5">{product.price}</Typography>
         </div>
+        <Typography variant="h2" color="textSecondary"></Typography>
       </CardContent>
     </Card>
   );
