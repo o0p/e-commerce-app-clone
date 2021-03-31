74cbccbf16d49783ab34f8e60f5f163121294de3
Ruslan <rus@gmx.com>
2021-03-31 23:02:55 +0100

    Import Product

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Grid, { Typography } from '@material-ui/core';
-import classes from '*.module.css';
+import Grid from '@material-ui/core';
+import Product from './Product/Product'
 
 const products = [
   { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5' },
@@ -12,14 +12,7 @@ const Products = () => {
     <Grid container justify="center" spacing={4}>
       {products.map((product) => {
         <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-          <Products product={product} />
-          {/* <CardContent>
-            <div className={classes.CardContent}>
-              <Typography variant="h5" gutterBottom>
-                {product.name}
-              </Typography>
-            </div>
-          </CardContent> */}
+          <Product product={product} />
         </Grid>;
       })}
     </Grid>
