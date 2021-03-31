35e4660ef9fec9c79c99c6fff603a37c8eaac34b
Ruslan <rus@gmx.com>
2021-03-31 01:00:19 +0100

    Add .cardContent style

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -11,5 +11,9 @@ export default makeStyles(() => ({
     cardActions: {
         display: 'flex',
         justifyContent: 'flex-end',
-    }
+    },
+    cardContent: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
 }));
