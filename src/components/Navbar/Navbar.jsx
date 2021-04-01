fd4616801af4cc26938d17a4574044b9828eb56e
Ruslan <rus@gmx.com>
2021-04-01 23:56:27 +0100

    Add shopping cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,8 +7,11 @@ import {
   MenuItem,
   Menu,
   Typography,
+  Icon,
 } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
+
+import logo from '../../assets/online-shopping.png ';
 import classes from '*.module.css';
 
 const Navbar = () => {
@@ -18,13 +21,21 @@ const Navbar = () => {
         <Toolbar>
           <Typography variant="h6" className={classes.title} color="inherit">
             <img
-              src={}
+              src={logo}
               alt="Ruslan' s Shop"
               height="25px"
               className={classes.image}
             />
             Ruslan's Shop
           </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton area-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
     </>
